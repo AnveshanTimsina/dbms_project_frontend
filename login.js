c30bc2f22e407844c458c789de02328fa60e22df
Anveshan Timsina <timsinaanveshan@gmail.com>
2024-08-10 10:58:25 +0545

    fix attempt 2

--- login.js
+++ login.js
@@ -11,7 +11,7 @@ const login = () => {
 
   async function getUser() {
     try {
-      const res = await apiFetch(`/login`, {
+      const res = await apiFetch(`login`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
