b1fe96f46ecb80e9ef54970f5e7eb0b1a9ecc07e
Anveshan Timsina <timsinaanveshan@gmail.com>
2024-08-10 10:30:07 +0545

    fix attempt 6

--- api.js
+++ api.js
@@ -1,7 +1,11 @@
 const apiFetch = async (url, options) => {
-  const configResponse = await fetch("/config.json");
-  const config = await configResponse.json();
-  const API_URL = config.API_URL;
+  const myUrl = `${window.location.host}`;
+  // console.log(myUrl);
+  // const configResponse = await fetch("/config.json");
+  // const config = await configResponse.json();
+  const API_URL = myUrl.includes("vercel")
+    ? "https://backend-steel-three.vercel.app"
+    : "http://localhost:8000";
 
   return fetch(`${API_URL}/${url}`, options);
 };
